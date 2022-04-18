6be50b27f39186332762b4f8763fffa4b9e619a3
itishprasad30 <itishprasad20@gmail.com>
2022-04-18 20:41:01 +0530

    home page layout is done

--- components/Row.tsx
+++ components/Row.tsx
@@ -1,7 +1,57 @@
-import React from 'react'
+import { ChevronLeftIcon, ChevronRightIcon } from '@heroicons/react/outline'
+import * as React from 'react'
+import { Movie } from '../typing'
+import Thumbnail from './Thumbnail'
 
-const Row = () => {
-  return <div>Row</div>
+interface Props {
+  title: string
+  movies: Movie[]
+}
+
+const Row = ({ title, movies }: Props) => {
+  const rowRef = React.useRef<HTMLDivElement>(null)
+  const [isMoved, setMoved] = React.useState(false)
+
+  const handleClick = (direction: string) => {
+    setMoved(true)
+    if (rowRef.current) {
+      const { scrollLeft, clientWidth } = rowRef.current
+      const scrollTo =
+        direction === 'left'
+          ? scrollLeft - clientWidth
+          : scrollLeft + clientWidth
+
+      rowRef.current.scrollTo({ left: scrollTo, behavior: 'smooth' })
+    }
+  }
+
+  return (
+    <div className="h-40 space-y-0.5 md:space-y-2">
+      <h2 className="w-56 cursor-pointer text-sm font-semibold text-[#e5e5e5] transition duration-200 hover:text-white md:text-2xl">
+        {title}
+      </h2>
+      <div className="group relative md:-ml-2">
+        <ChevronLeftIcon
+          className={`absolute top-0 bottom-0 left-2 z-40 m-auto h-9 w-9 cursor-pointer opacity-0 transition hover:scale-125 group-hover:opacity-100 ${
+            !isMoved && 'hidden'
+          }`}
+          onClick={() => handleClick('left')}
+        />
+        <div
+          className="flex items-center space-x-0.5 overflow-x-scroll scrollbar-hide  md:space-x-2.5 md:p-2"
+          ref={rowRef}
+        >
+          {movies.map((movie) => (
+            <Thumbnail key={movie.id} movie={movie} />
+          ))}
+        </div>
+        <ChevronRightIcon
+          className="absolute top-0 bottom-0 right-2 z-40 m-auto h-9 w-9 cursor-pointer opacity-0 transition hover:scale-125 group-hover:opacity-100"
+          onClick={() => handleClick('right')}
+        />
+      </div>
+    </div>
+  )
 }
 
 export default Row
--- pages/index.tsx
+++ pages/index.tsx
@@ -29,7 +29,7 @@ const Home = ({
   trendingNow,
 }: Props) => {
   return (
-    <div className="relative h-screen bg-gradient-to-b from-gray-900/10 to-[#010511] lg:h-[140vh]  ">
+    <div className="relative h-screen bg-gradient-to-b  lg:h-[140vh]  ">
       <Head>
         <title>Home-Netflix</title>
         <link rel="icon" href="/favicon.ico" />
@@ -39,12 +39,18 @@ const Home = ({
       <main className="relative pl-4 pb-24 lg:space-y-24 lg:pl-16">
         {/* Banner */}
         <Banner netflixOriginals={netflixOriginals} />
-        <section>
+        <section className="space-y-24">
           {/* row */}
-          <Row />
-          <Row />
-          <Row />
-          <Row />
+          <Row title="Trending Now" movies={trendingNow} />
+          <Row title="Top Rated" movies={topRated} />
+          <Row title="Action Thrillers" movies={actionMovies} />
+          {/* My List */}
+          {/* {list.length > 0 && <Row title="My List" movies={list} />} */}
+
+          <Row title="Comedies" movies={comedyMovies} />
+          <Row title="Scary Movies" movies={horrorMovies} />
+          <Row title="Romance Movies" movies={romanceMovies} />
+          <Row title="Documentaries" movies={documentaries} />
           {/* row */}
           {/* row */}
           {/* row */}
