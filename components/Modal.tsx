d23d6bc155b0f3555db86421b6cb01f2f7f76edf
itishprasad30 <itishprasad20@gmail.com>
2022-04-19 20:15:39 +0530

    tailer modal done

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -4,12 +4,21 @@ import { useRecoilState } from 'recoil'
 import { modalState, movieState } from '../atoms/modalAtom'
 import { XIcon } from '@heroicons/react/solid'
 import { Movie, Element, Genre } from '../typing'
+import ReactPlayer from 'react-player/lazy'
+import { FaPlay } from 'react-icons/fa'
+import {
+  PlusIcon,
+  ThumbUpIcon,
+  VolumeOffIcon,
+  VolumeUpIcon,
+} from '@heroicons/react/outline'
 
 const Modal = () => {
   const [movie, setMovie] = useRecoilState(movieState)
   const [showModal, setShowModal] = useRecoilState(modalState)
   const [trailer, setTrailer] = useState('')
   const [genres, setGenres] = useState<Genre[]>([])
+  const [muted, setMuted] = useState(false)
 
   const handleClose = () => {
     setShowModal(false)
@@ -42,8 +51,13 @@ const Modal = () => {
 
     fetchMovie()
   }, [])
+
   return (
-    <MuiModal open={showModal} onClose={handleClose}>
+    <MuiModal
+      open={showModal}
+      onClose={handleClose}
+      className=" fixed !top-7 left-0 right-0 mx-auto w-full max-w-5xl overflow-hidden overflow-y-scroll rounded-md scrollbar-hide"
+    >
       <>
         <button
           onClick={handleClose}
@@ -51,6 +65,75 @@ const Modal = () => {
         >
           <XIcon className="h-6 w-6" />
         </button>
+
+        <div className="relative pt-[56.25%]">
+          <ReactPlayer
+            className="react-player"
+            url={`https://www.youtube.com/watch?v=${trailer}`}
+            width="100%"
+            height="100%"
+            style={{ position: 'absolute', top: '0', left: '0' }}
+            muted={muted}
+          />
+          <div className="absolute bottom-10 flex w-full items-center justify-between px-10">
+            <div className="flex space-x-2">
+              <button className="flex items-center gap-x-2 rounded bg-white px-8 text-xl font-bold text-black transition hover:bg-[#e6e6e6]">
+                <FaPlay className="h-7 w-7 text-black" />
+                Play
+              </button>
+
+              <button className="modalButton">
+                <PlusIcon className="h-7 w-7" />
+              </button>
+              <button className="modalButton">
+                <ThumbUpIcon className="h-7 w-7" />
+              </button>
+            </div>
+
+            <button onClick={() => setMuted(!muted)}>
+              {muted ? (
+                <VolumeOffIcon className="h-7 w-7" />
+              ) : (
+                <VolumeUpIcon className="h-7 w-7" />
+              )}
+            </button>
+          </div>
+        </div>
+
+        <div className="flex space-x-16 rounded-b-md bg-[#181818] px-10 py-8">
+          <div className="space-y-6 text-lg">
+            <div className="flex items-center space-x-2 text-sm">
+              <p className="font-semibold text-green-400">
+                {movie!.vote_average * 10}% Match
+              </p>
+              <p className="font-light">
+                {movie?.release_date || movie?.first_air_date}
+              </p>
+              <div className="flex h-4 items-center justify-center rounded border border-white/40 px-1.5 text-xs">
+                HD
+              </div>
+            </div>
+            <div className="flex flex-col gap-x-10 gap-y-4 font-light md:flex-row">
+              <p className="w-5/6">{movie?.overview}</p>
+              <div className="flex flex-col space-y-3 text-sm">
+                <div>
+                  <span className="text-[gray]">Genres:</span>{' '}
+                  {genres.map((genre) => genre.name).join(', ')}
+                </div>
+
+                <div>
+                  <span className="text-[gray]">Original language:</span>{' '}
+                  {movie?.original_language}
+                </div>
+
+                <div>
+                  <span className="text-[gray]">Total votes:</span>{' '}
+                  {movie?.vote_count}
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </>
     </MuiModal>
   )
--- components/Thumbnail.tsx
+++ components/Thumbnail.tsx
@@ -1,14 +1,22 @@
 import Image from 'next/image'
 import React from 'react'
+import { useRecoilState } from 'recoil'
+import { modalState, movieState } from '../atoms/modalAtom'
 import { Movie } from '../typing'
 interface Props {
   movie: Movie
 }
 
 const Thumbnail = ({ movie }: Props) => {
+  const [currentMovie, setCurrentMovie] = useRecoilState(movieState)
+  const [showModal, setShowModal] = useRecoilState(modalState)
   return (
     <div
       className={`relative h-28 min-w-[180px] cursor-pointer transition duration-200 ease-out md:h-36 md:min-w-[260px] md:hover:scale-105`}
+      onClick={() => {
+        setCurrentMovie(movie)
+        setShowModal(true)
+      }}
     >
       <Image
         src={`https://image.tmdb.org/t/p/w500${
@@ -17,6 +25,9 @@ const Thumbnail = ({ movie }: Props) => {
         className="rounded-sm object-cover md:rounded"
         layout="fill"
       />
+      <h2 className="absolute bottom-2 text-2xl font-semibold">
+        {movie?.title || movie.original_name}
+      </h2>
     </div>
   )
 }
